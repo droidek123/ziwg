302298a0eab88cad37da79eb73fb20762a908503
Kubagf <kubafg5@gmail.com>
2025-04-08 00:39:39 +0200

    remove unnecessary repository

--- Utils/src/main/java/pl/edu/pwr/zigw/repository/ReservationRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.pwr.zigw.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.edu.pwr.zigw.model.Reservation;
-
-@Repository
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-}
--- Utils/src/main/java/pl/edu/pwr/zigw/repository/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.pwr.zigw.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.edu.pwr.zigw.model.Ticket;
-
-@Repository
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}

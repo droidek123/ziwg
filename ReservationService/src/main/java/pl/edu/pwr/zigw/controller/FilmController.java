7cacd64c0dfc227b3a88b1c782ba064e0081e8b3
droidek <droidek123@interia.pl>
2025-04-08 09:33:32 +0200

    Work in progress

---
+++ ReservationService/src/main/java/pl/edu/pwr/zigw/controller/FilmController.java
@@ -0,0 +1,7 @@
+package pl.edu.pwr.zigw.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilmController {
+}
--- ReservationService/src/main/java/pl/edu/pwr/zigw/controller/ReservationController.java
+++ ReservationService/src/main/java/pl/edu/pwr/zigw/controller/ReservationController.java
@@ -3,9 +3,11 @@ package pl.edu.pwr.zigw.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import models.Reservation;
 import org.springframework.web.bind.annotation.*;
+import pl.edu.pwr.zigw.model.Reservation;
 import pl.edu.pwr.zigw.service.ReservationService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/reservation")
@@ -17,31 +19,31 @@ public class ReservationController {
 
     @GetMapping("/reservations")
     @Operation(summary = "Get all reservations", description = "Returns a list or summary of all reservations.")
-    public Reservation getReservations() {
+    public List<Reservation> getReservations() {
         return this.reservationService.getReservations();
     }
 
     @GetMapping
     @Operation(summary = "Get reservation by ID", description = "Retrieves a single reservation based on the provided ID.")
     public Reservation getReservation(@RequestParam Long id) {
-        return this.reservationService.getReservations();
+        return this.reservationService.getReservationById(id);
     }
 
-    @PostMapping
+    @PostMapping("reservations/new")
     @Operation(summary = "Create a new reservation", description = "Adds a new reservation and returns its ID.")
-    public Long addReservation() {
-        return 1L; // placeholder - should call reservationService.addReservation(...)
+    public Long addReservation(@RequestBody Reservation reservation) {
+        return reservationService.addReservation(reservation);
     }
 
     @PutMapping
     @Operation(summary = "Update an existing reservation", description = "Updates the reservation with the given ID and returns the updated reservation.")
-    public Reservation updateReservation(@RequestParam Long id) {
-        return this.reservationService.getReservations();
+    public Reservation updateReservation(@RequestBody Reservation reservation) {
+        return this.reservationService.updateReservation(reservation);
     }
 
     @DeleteMapping
     @Operation(summary = "Delete a reservation", description = "Deletes the reservation with the specified ID and returns whether the operation was successful.")
     public Boolean deleteReservation(@RequestParam Long id) {
-        return true;
+        return reservationService.deleteReservation(id);
     }
 }

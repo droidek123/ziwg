ab489737826278faf1cbc88b221a684c17c5d41c
Kubagf <kubafg5@gmail.com>
2025-04-20 14:06:50 +0200

    Basic User API

---
+++ UserService/src/main/java/pl/edu/pwr/zigw/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package pl.edu.pwr.zigw.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@OpenAPIDefinition
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Service")
+                        .version("1.0")
+                        .description("Api documentation for user service"));
+    }
+}

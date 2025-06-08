97aa02d8feb2865cf78f34889b188933667542b4
Boris Komarov <boris.komarov@proton.me>
2025-06-08 12:04:44 +0200

    My tickets view added

--- web/src/App.js
+++ web/src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './Register';
 import Login from './Login'; 
-import ReservationView from './ReservationView';
 import SnackView from './SnackView';
 import FilmDescription from './FilmDescrption';
-import TicketView from './TicketView';
 import Home from './Home';
 import Account from './Account';
 import PlaceReservationView from './PlaceReservationView';
+import TicketView from './TicketView';
+import MyTickets from './MyTickets';
+import ReservationView from './ReservationView';
 import Confirmation from './Confirmation';
-
 
 function App() {
   return (
@@ -19,16 +19,46 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/reservation" element={<ReservationView />} />
-        <Route path="/ordersnacks" element={<SnackView />} />
-        <Route path="/filmdetails" element={<FilmDescription />} />
-        <Route path="/ticket" element={<TicketView />} />
         <Route path="/account" element={<Account />} />
+        <Route path="/mytickets" element={<MyTickets />} />
+        <Route path="/ticket" element={<TicketView />} />
+        <Route path="/filmdetails" element={<FilmDescription />} />
+        <Route path="/ordersnacks" element={<SnackView />} />
         <Route path="/placereservation" element={<PlaceReservationView />} />
-        <Route path="/confirmation" element={<Confirmation />} />
+        {/* it is decided that no use for them */}
+        {/* <Route path="/reservation" element={<ReservationView />} /> */}
+        {/* <Route path="/confirmation" element={<Confirmation />} /> */}
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
+// Reservation procedure
+//
+// Login / Register
+// V
+// Home page
+// I
+// V -- choose film
+// I
+// Film details
+// I
+// V -- reserve pressed
+// I
+// Choose seat
+// I
+// V -- Reserved back to home
+// I
+// Home page
+
+// View your reservations procedure
+// 
+// Login / Register
+// V
+// Select My Tickets (Opens view with all tickets)
+// I
+// V -- chose needed one
+// I
+// opens chosen ticket with more info inside, and from here is possible to order snacks

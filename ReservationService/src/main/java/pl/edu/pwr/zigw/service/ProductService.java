50ccadf30c02c90cc8d6edc02ed12bba0862143d
Kubagf <kubafg5@gmail.com>
2025-06-10 21:30:53 +0200

    Add image upload, retrieval, and deletion functionality for products

--- ReservationService/src/main/java/pl/edu/pwr/zigw/service/ProductService.java
+++ ReservationService/src/main/java/pl/edu/pwr/zigw/service/ProductService.java
@@ -2,6 +2,7 @@ package pl.edu.pwr.zigw.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.edu.pwr.zigw.model.Movie;
 import pl.edu.pwr.zigw.model.Product;
 import pl.edu.pwr.zigw.repostiory.ProductRepository;
 
@@ -15,5 +16,13 @@ public class ProductService {
 
     public List<Product> getAllProducts() {
         return productRepository.findAll();
+    }
+
+    public Product findById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public void save(Product product) {
+        productRepository.save(product);
     }
 }
--- Utils/src/main/java/pl/edu/pwr/zigw/model/Product.java
+++ Utils/src/main/java/pl/edu/pwr/zigw/model/Product.java
@@ -22,4 +22,6 @@ public class Product {
     private Long number;
 
     private Float price;
+
+    private String imageFilename;
 }
